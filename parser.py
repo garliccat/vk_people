755e0a077a009079744a24f69074d36b7e41ba86
Anton Petkov <petkov.anton.o@gmail.com>
2020-10-13 22:42:08 +0300

    almost finished

--- parser.py
+++ parser.py
@@ -5,34 +5,18 @@ import os.path
 
 
 def write_csv(data):
-    with open('vk_friends.csv', 'a', newline='', encoding='utf-8') as f:
+    with open('vk_people.csv', 'a', newline='', encoding='utf-8') as f:
         #newline - to avoid blank rows after each record
         #encoding utf-16 - we are in russia, thats all
         writer = csv.writer(f, delimiter=';')
         writer.writerow(data)
 
 def uid_info(uid):
-    ret = requests.get(r'https://api.vk.com/method/users.get?user_id={}&access_token={}&fields=country,city,sex&v=5.124'.format(uid, token)).json()
+    ret = requests.get(r'https://api.vk.com/method/users.get?user_id={}&access_token={}&fields=country,city,sex,bdate,music,movies&v=5.124'.format(uid, token)).json()
     return ret
 
-# def uid_frnds(uid):
-#     '''
-#     returns a list of names of friends of provided VK uid
-#     '''
-#     ret = requests.get(r'https://api.vk.com/method/friends.get?user_id={}&access_token={}&v=5.124'.format(uid, token)).json()
-#     if ret['response']['count'] > 0:
-#         return [uid_name(i) for i in ret['response']['items']]
-
-# def uid_frndscnt(uid):
-#     '''
-#     returns friends count from UID
-#     '''
-#     ret = requests.get(r'https://api.vk.com/method/friends.get?user_id={}&access_token={}&v=5.124'.format(uid, token)).json()
-#     return ret['response']['count']
-
-
 if os.path.exists('vk_people.csv'):
-    with open('vk_friends.csv', 'r', encoding='utf-8') as f:
+    with open('vk_people.csv', 'r', encoding='utf-8') as f:
         l = 1
         for i in f:
             l += 1
@@ -42,16 +26,24 @@ if os.path.exists('vk_people.csv'):
         print('Number of lines: %d' % l)
 else:
     l = 1
-    last_uid = 440000000
+    last_uid = 0
 
-stop = last_uid + 1000000
+stop = 619000000
 
 for uid in range(last_uid + 1, stop):
     
     rawdata = uid_info(uid)
 
     try:
+        if rawdata['response'][0]['deactivated']:
+            continue
+    except:
+        pass
+
+    try:
         name = str(rawdata['response'][0]['first_name'])
+        if name == 'DELETED':
+            continue
     except:
         name = ''
         
@@ -62,20 +54,49 @@ for uid in range(last_uid + 1, stop):
 
     try:
         sex = rawdata['response'][0]['sex']
-
-        
-        l += 1
-        print('Line number: ', l)
-        print('UID: ', uid, ' of ', stop, ' ({:.1f} %)'.format((uid - last_uid) / (stop - last_uid) * 100))
-        print('Name: ', name)
-        print('Last Name: ', last_name)
-        print('Sex: ', sex)
-        print()
-
-        # write_csv([uid, 
-        #     name, 
-        #     frnds_cnt, 
-        #     '|'.join(frnds_lst)])
-
     except:
-       pass
+        sex = ''
+    
+    try:
+        country = rawdata['response'][0]['country']['title']
+    except:
+        country = ''
+    
+    try:
+        city = rawdata['response'][0]['city']['title']
+    except:
+        city = ''
+
+    try:
+        bdate = rawdata['response'][0]['bdate']
+    except:
+        bdate = ''
+    
+    try:
+        music = rawdata['response'][0]['music']
+    except:
+        music = ''
+    
+    try:
+        movies = rawdata['response'][0]['movies']
+    except:
+        movies = ''
+    
+    l += 1
+
+    print('Line number: ', l)
+    print('UID: ', uid, ' of ', stop, ' ({:.1f} %)'.format((uid - last_uid) / (stop - last_uid) * 100))
+    print('Name: ', name)
+    print('Last Name: ', last_name)
+    print('Sex: ', sex)
+    print('Country: ', country)
+    print('City: ', city)
+    print('Birth date: ', bdate)
+    print('Music: ', music)
+    print('Movies: ', movies)
+    print()
+
+    # write_csv([uid, 
+    #     name, 
+    #     frnds_cnt, 
+    #     '|'.join(frnds_lst)])
